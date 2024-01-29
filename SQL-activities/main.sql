4c9f1f7e8fb441b3ffd745912004f7fb1470940a
Erollercoaster <erollfern@gmail.com>
2024-01-29 20:39:34 +0800

    Add SQL Activities

--- SQL-activities/main.sql
+++ SQL-activities/main.sql
@@ -1,12 +1,26 @@
-CREATE TABLE lessons3 (
+CREATE TABLE students2 (
 	id integer PRIMARY KEY,
-	name character varying(255) NOT NULL,
-	description text,
-	front_end boolean DEFAULT false,
-	back_end boolean DEFAULT false,
-	price numeric(8, 2) DEFAULT 0.0,
-	created_at timestamp without time zone NOT NULL,
-	updated_at timestamp without time zone NOT NULL
+	first_name varchar(100) NOT NULL,
+	middle_name text,
+	last_name varchar(100) NOT NULL,
+	age integer,
+	location varchar(100) NOT NULL
 );
 
-SELECT * FROM lessons3;
+SELECT * FROM students2;
+
+INSERT INTO students2 (id, first_name, middle_name, last_name, age, location)
+VALUES 
+(1, 'Juan', 'Blank', 'Cruz', 18, 'Manila'),
+(2, 'Anne', 'Blank', 'Wall', 20, 'Manila'),
+(3, 'Theresa', 'Blank', 'Joseph', 21, 'Manila'),
+(4, 'Isaac', 'Blank', 'Gray', 19, 'Laguna'),
+(5, 'Zack', 'Blank', 'Matthews', 22, 'Marikina'),
+(6, 'Finn', 'Blank', 'Lam', 25, 'Manila');
+
+UPDATE students2
+SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan'
+WHERE id = 6;
+
+DELETE FROM students2
+WHERE id = (SELECT MAX(id) FROM students);
